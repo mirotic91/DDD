f6d27d84551cba4565454b2b9339768cd3e93122
박종욱 <jonguk1221@gmail.com>
2019-02-17 16:54:21 +0900

    배송지/출고상태 변경

--- src/main/java/com/mirotic91/demo/order/domain/Order.java
+++ src/main/java/com/mirotic91/demo/order/domain/Order.java
@@ -8,4 +8,20 @@ public class Order {
     private OrderState state;
 
     private ShippingInfo shippingInfo;
+
+    public void changeShippingInfo(ShippingInfo newShippingInfo) {
+        if (!state.isShippingChangeable()) {
+            throw new IllegalStateException(String.format("can't change order shipping in state[%s]", state));
+        }
+
+        this.shippingInfo = newShippingInfo;
+    }
+
+    public void changeShipped() {
+        if (state != OrderState.PREPARING) {
+            throw new IllegalStateException(String.format("can't change orderState shipped in state[%s]", state));
+        }
+        this.state = OrderState.SHIPPED;
+    }
+
 }
--- src/main/java/com/mirotic91/demo/order/domain/OrderState.java
+++ src/main/java/com/mirotic91/demo/order/domain/OrderState.java
@@ -1,5 +1,10 @@
 package com.mirotic91.demo.order.domain;
 
 public enum OrderState {
+
     PAYMENT_WAITING, PREPARING, SHIPPED, DELIVERING, DELIVERY_COMPLETED;
+
+    public boolean isShippingChangeable() {
+        return this == PAYMENT_WAITING || this == PREPARING;
+    }
 }
