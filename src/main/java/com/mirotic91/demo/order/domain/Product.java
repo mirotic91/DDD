c999bb966428fab9b3806d63f1b60252aab0ab58
박종욱 <jonguk1221@gmail.com>
2019-02-19 23:36:00 +0900

    Product - 필드 추가

--- src/main/java/com/mirotic91/demo/order/domain/Product.java
+++ src/main/java/com/mirotic91/demo/order/domain/Product.java
@@ -1,5 +1,13 @@
 package com.mirotic91.demo.order.domain;
 
+import lombok.Getter;
+
+@Getter
 public class Product {
 
+    private String name;
+
+    private Money price;
+
+    private String detail;
 }
