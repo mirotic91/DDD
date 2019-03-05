7338e1745e474a39c2069761e249f1bc601f97b9
박종욱 <jonguk1221@gmail.com>
2019-03-06 08:28:34 +0900

    주문자 컬럼화

--- src/main/java/com/mirotic91/demo/order/domain/OrderNo.java
+++ src/main/java/com/mirotic91/demo/order/domain/OrderNo.java
@@ -1,10 +1,24 @@
 package com.mirotic91.demo.order.domain;
 
+import lombok.AccessLevel;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+import javax.persistence.Access;
+import javax.persistence.AccessType;
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+import java.io.Serializable;
+
+@Embeddable
 @Getter
-public class OrderNo {
+@Access(AccessType.FIELD)
+@EqualsAndHashCode(of = "id")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class OrderNo implements Serializable {
 
-    private String value;
+    @Column(name = "order_number")
+    private String number;
 
 }
--- src/main/java/com/mirotic91/demo/order/domain/Orderer.java
+++ src/main/java/com/mirotic91/demo/order/domain/Orderer.java
@@ -2,10 +2,16 @@ package com.mirotic91.demo.order.domain;
 
 import lombok.Getter;
 
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+
+@Embeddable
 @Getter
 public class Orderer {
 
+    @Column(name = "orderer_name")
     private String name;
 
+    @Column(name = "orderer_email")
     private String email;
 }
