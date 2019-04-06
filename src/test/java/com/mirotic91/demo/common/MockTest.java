7178bcfedd3e86218b9dc6a7bfbf9001a36364c3
박종욱 <jonguk1221@gmail.com>
2019-04-06 23:49:44 +0900

    mockito extension

---
+++ src/test/java/com/mirotic91/demo/common/MockTest.java
@@ -0,0 +1,9 @@
+package com.mirotic91.demo.common;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+public class MockTest {
+
+}
