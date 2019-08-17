11a7c6550ef66a5a66a1d22add3d3cd154cd6cb0
박종욱 <jonguk1221@gmail.com>
2019-08-17 23:44:53 +0900

    api test config

---
+++ src/test/java/com/mirotic91/demo/common/IntegrationTest.java
@@ -0,0 +1,10 @@
+package com.mirotic91.demo.common;
+
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class IntegrationTest {
+
+}
---
+++ src/test/java/com/mirotic91/demo/common/MockApiTest.java
@@ -0,0 +1,22 @@
+package com.mirotic91.demo.common;
+
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+
+@WebMvcTest
+public class MockApiTest {
+
+    protected MockMvc mvc;
+
+    protected MockMvc buildMockMvc(WebApplicationContext context) {
+        return MockMvcBuilders
+                .webAppContextSetup(context)
+                .alwaysDo(print())
+                .build();
+    }
+
+}
